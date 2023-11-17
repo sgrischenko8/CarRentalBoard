7cab53c2bed9882d7157418478412f3f553e476a
sgrischenko8 <sgrischenko8@gmail.com>
2023-11-17 19:16:28 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
-import { persistor, store } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-import App from "./App.jsx";
-import "./index.css";
-import Loader from "./components/Loader/Loader";
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App.jsx';
+import './index.css';
+import Loader from './components/Loader/Loader';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<Loader />} persistor={persistor}>
         <BrowserRouter basename="/CarRentalBoard">
           <App />
-        </HashRouter>
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 // persistor.purge();
