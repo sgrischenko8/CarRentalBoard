a639946d93c87dd6265683dd97be2af7d0a9dbc8
sgrischenko8 <120989472+sgrischenko8@users.noreply.github.com>
2023-11-17 20:14:59 +0000

    Deploying to gh-pages from @ sgrischenko8/CarRentalBoard@5edab8a8e277bb04f0f80d0d2748fb343ec7ed17 🚀

--- assets/Favorites-Bcrcapgh.js
+++
@@ -1,1 +0,0 @@
-import{j as s,L as r}from"./index-jE6NWZK9.js";const e=()=>s.jsxs(s.Fragment,{children:[s.jsx("p",{children:"Favorites test"}),s.jsx(r,{})]});export{e as default};
---
+++ assets/Favorites-a3f6d98f.js
@@ -0,0 +1,1 @@
+import{j as s,L as r}from"./index-650debd6.js";const e=()=>s.jsxs(s.Fragment,{children:[s.jsx("p",{children:"Favorites test"}),s.jsx(r,{})]});export{e as default};
