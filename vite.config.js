6e677599a5989b03ad6dc2b47e40a5002e13a0f2
sgrischenko8 <sgrischenko8@gmail.com>
2023-11-17 19:20:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: "/src",
+      src: '/src',
     },
   },
-  base: "/CarRentalBoard",
+  base: '/CarRentalBoard',
+  build: {
+    target: 'esnext',
+  },
 });
