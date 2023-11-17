70e938d7529a4433dbf4a544851318e72c595071
sgrischenko8 <sgrischenko8@gmail.com>
2023-11-17 22:20:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 // import svgr from 'vite-plugin-svgr';
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
-  plugins: [react(), pluginRewriteAll()],
+  plugins: [react()],
   resolve: {
     alias: {
       src: '/src',
