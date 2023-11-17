95a24b9fa9dd6579b7b92907742f1008661b1e1b
sgrischenko8 <sgrischenko8@gmail.com>
2023-11-17 21:41:12 +0200

    Update Favorites.jsx

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,6 +1,6 @@
 // import { useState, useEffect, useRef } from "react";
 // import { useSearchParams } from "react-router-dom";
-import Loader from "src/components/Loader/Loader";
+import Loader from 'src/components/Loader/Loader';
 // import styles from "./Movies.module.css";
 
 const Favorites = () => {
@@ -18,7 +18,7 @@ const Favorites = () => {
 
   return (
     <>
-      <p>Favorites</p>
+      <p>Favorites test</p>
       <Loader />
     </>
   );
