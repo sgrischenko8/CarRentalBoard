fff87e56043687fb34850d0c66629a0ba0a7d886
sgrischenko8 <sgrischenko8@gmail.com>
2023-11-17 19:34:43 +0200

    Updates

--- assets/Favorites-11935c91.js
+++
@@ -1,1 +0,0 @@
-import{j as r,L as s}from"./index-5be2cc79.js";const t=()=>r.jsxs(r.Fragment,{children:[r.jsx("p",{children:"Favorites"}),r.jsx(s,{})]});export{t as default};
---
+++ assets/Favorites-a3b25d95.js
@@ -0,0 +1,1 @@
+import{j as r,L as s}from"./index-7b5dfe19.js";const t=()=>r.jsxs(r.Fragment,{children:[r.jsx("p",{children:"Favorites"}),r.jsx(s,{})]});export{t as default};
