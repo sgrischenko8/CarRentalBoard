9de1bb57b5d06bbe9aac531138fcf4534cb40048
sgrischenko8 <120989472+sgrischenko8@users.noreply.github.com>
2023-11-20 19:31:15 +0000

    Deploying to gh-pages from @ sgrischenko8/CarRentalBoard@aca4be0fa07cc5a2ffc99a29e30c17ce1f50ec5a 🚀

---
+++ assets/Favorites-CQgai6q4.js
@@ -0,0 +1,1 @@
+import{u as o,s as i,a as n,j as s}from"./index-hzBX05Js.js";import{C as c,M as l}from"./Message-CEcj1LHY.js";const m=()=>{const a=o(i).favorites,e=n().state.allCars;let t=[];return e&&(t=e.filter(r=>a.includes(r._id))),s.jsx(s.Fragment,{children:t.length>0?s.jsx(c,{cars:t}):s.jsx(l,{string:"Your favorites is empty now"})})};export{m as default};
